f944f3083728e78eae2dafc9760c47b37773f74e
Basty001 <bas.sotog@duocuc.cl>
2025-11-18 00:45:47 -0300

    ..

--- Usuarios/src/main/java/com/qualifygym/usuarios/controller/UsuarioController.java
+++ Usuarios/src/main/java/com/qualifygym/usuarios/controller/UsuarioController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.qualifygym.usuarios.model.Usuario;
@@ -70,8 +71,8 @@ public class UsuarioController {
             @ApiResponse(responseCode = "404", description = "Usuario no encontrado"),
             @ApiResponse(responseCode = "500", description = "Error interno del servidor")
     })
-    @GetMapping("/users/email/{email}")
-    public ResponseEntity<?> getUsuarioByEmail(@PathVariable String email) {
+    @GetMapping("/users/email")
+    public ResponseEntity<?> getUsuarioByEmail(@RequestParam String email) {
         try {
             Usuario usuario = usuarioService.obtenerUsuarioPorEmail(email);
             return ResponseEntity.ok(usuario);
